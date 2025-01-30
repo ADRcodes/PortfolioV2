70ce4f2064d437fb3581382bb8f735f59982819e
Alex Russell <alexrussellria@gmail.com>
2025-01-30 15:23:07 -0330

    Minor edits to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,14 +3,14 @@
 const About = () => {
   return (
     <section className="flex items-center pt-2 md:pt-10 pb-2">
-      <div className="container mx-auto px-4 md:px-8 grid md:grid-cols-2 gap-8 items-center">
+      <div className="container mx-auto px-4 md:px-8 grid lg:grid-cols-2 gap-8 items-center">
         {/* Left Column: Text Content */}
         <div className="space-y-6">
           <div className="flex justify-center pb-4">
             <img
               src="images/profile-with-background.png"
               alt="Alex Russell"
-              className="w-48 h-48 block lg:hidden object-cover rounded-full"
+              className="w-48 h-48 md:w-[350px] md:h-[250px] block lg:hidden object-cover rounded-full"
             />
           </div>
           <div>
@@ -49,7 +49,7 @@ const About = () => {
           <img
             src="images/profile-with-background.png"
             alt="Alex Russell"
-            className="w-48 h-48 hidden lg:block object-cover rounded-full"
+            className="w-[300px] h-[300px] hidden lg:block object-cover rounded-ss-[50%] rounded-ee-[50%] rounded-es-[200px] rounded-se-[200px]"
           />
         </div>
       </div>
