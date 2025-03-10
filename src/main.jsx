c8d925d0dceea367cbb8ab5f4f6738df532bb1de
Alex Russell <alexrussellria@gmail.com>
2025-03-10 15:11:54 -0230

    Fixed css file import cus it hates capitals

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './styles/main.css'
-import '/src/styles/ImageSlider.css'
+import '/src/styles/imageSlider.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
