ceb644e8e6a80b6eb9f33b19bd07663da7b05025
Alex Russell <alexrussellria@gmail.com>
2025-03-10 16:08:58 -0230

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,8 @@ import About from "../components/About"
 import Contact from "../components/Contact"
 import ImageSlider from "../components/IconSlider"
 import Projects from "../components/projects/Projects"
-import StickyTop from "../components/StickyTop/StickyTop"
+import StickyTop from "../components/stickyTop/StickyTop"
+
 
 const Home = () => {
   return (
