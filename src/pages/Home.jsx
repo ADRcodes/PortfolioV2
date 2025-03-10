d9200e331ad0540f98a5fc41e278075c285c4317
Alex Russell <alexrussellria@gmail.com>
2025-03-10 16:05:33 -0230

    Removed unused route from home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import About from "../components/About"
 import Contact from "../components/Contact"
-import ExperienceTimeline from "../components/experienceTimeline.jsx/ExperienceTimeline"
 import ImageSlider from "../components/IconSlider"
 import Projects from "../components/projects/Projects"
 import StickyTop from "../components/StickyTop/StickyTop"
@@ -12,7 +11,6 @@ const Home = () => {
       <About />
       <ImageSlider />
       <Projects />
-      {/* <ExperienceTimeline /> */}
       <Contact />
     </div>
   )
